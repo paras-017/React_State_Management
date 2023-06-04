ff6b48f148167b4d249a56e56e122b80d1c59b64
paras-017 <paraschauhan0017@gmail.com>
2023-06-04 12:28:40 +0530

    useCallBack

--- native-useMemo_useCallback/src/App.jsx
+++ native-useMemo_useCallback/src/App.jsx
@@ -1,16 +1,37 @@
-import { useMemo, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import './App.css'
+
+function SortList({list, sortFunc}){
+    console.log('SortList render');
+    const sortedList = useMemo(()=>{
+        console.log('running');
+       return [...list].sort(sortFunc)
+    }, [list, sortFunc])
+    return (
+        <div>
+            {sortedList.join(', ')}
+        </div>
+    )
+}
 
 function App(){
  const [numbers ] = useState([2434,3344,9345,4556,8943,123,4453])
- const [list] = useState(['paras','singh','hello','there','adrian'])
- const total = useMemo(()=>numbers.reduce((acc, number)=>acc+number, 0), [numbers])
-const sortName = useMemo(()=>[...list].sort(), [list])
+ const [names] = useState(['paras','singh','hello','there','adrian'])
+ let [count, setCount] = useState(0)
+const total = useMemo(()=>numbers.reduce((acc, number)=>acc+number, 0), [numbers])
+const sortName = useMemo(()=>[...names].sort(), [names])
+const sortFunc = useCallback((a,b)=>a.localeCompare(b),[])
+
+const addOne=()=>setCount(++count)
+
 
  return (
+    
   <>
   <div>Total:{total}</div>
-  <div>sortName:{sortName.join(', ')}</div>
+  {/* <div>sortName:{sortName.join(', ')}</div> */}
+    <SortList list={names} sortFunc={sortFunc}/>
+    <button onClick={addOne}>count:{count}</button>
   </>
  )
 }
