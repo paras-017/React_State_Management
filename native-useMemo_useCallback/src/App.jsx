bbbb6de61dcb7a14f970ab190b0fea674c2a2cc4
paras-017 <paraschauhan0017@gmail.com>
2023-06-04 11:53:00 +0530

    useMemo

--- native-useMemo_useCallback/src/App.jsx
+++ native-useMemo_useCallback/src/App.jsx
@@ -1,56 +1,18 @@
-
+import { useMemo, useState } from 'react'
 import './App.css'
-import { useReducer } from 'react'
 
+function App(){
+ const [numbers ] = useState([2434,3344,9345,4556,8943,123,4453])
+ const [list] = useState(['paras','singh','hello','there','adrian'])
+ const total = useMemo(()=>numbers.reduce((acc, number)=>acc+number, 0), [numbers])
+const sortName = useMemo(()=>[...list].sort(), [list])
 
-function UserForm(){
-  const [state, dispatch] = useReducer((state, action)=>({
-    ...state, ...action
-  }),{
-    first:'',
-    last:''
-  })
-
-  return(
-    <>
-    <input type="text" value = {state.first} onChange={e=>dispatch({first:e.target.value})}/>
-    <input type="text" value = {state.last} onChange={e=>dispatch({last:e.target.value})}/>
-    <div>
-    <div>first:{state.first}</div>
-    <div>first:{state.first}</div>
-    </div>
-    </>
-  )
-}
-
-function Form(){
-  const [state, dispatch] = useReducer((state, action)=>{
-    switch(action.type){
-      case 'Set_Name': return {...state, name: action.payload}
-      case 'Add_Name': return {...state, names: [...state.names,state.name], name:'',}
-    }
-  }, {names:['jac', 'john','jill'],name:""})
-  
-    return (
-      <>
-      <ul>
-        { state.names.map((name, index)=>(
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-        <input type="text" value={state.name} onChange={e=>dispatch({type:'Set_Name' ,payload:(e.target.value)})}/>
-        <button onClick={()=>dispatch({type:'Add_Name'})}>submit</button>
-      </>
-    )
-}
-
-function App() {
-return (
+ return (
   <>
-  {/* <Form/> */}
-  <UserForm/>
+  <div>Total:{total}</div>
+  <div>sortName:{sortName.join(', ')}</div>
   </>
-)
+ )
 }
 
 export default App
